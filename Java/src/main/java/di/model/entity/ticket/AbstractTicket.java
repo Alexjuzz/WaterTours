369ef43967e1272b0b97a375988672be8532190f
Alexjuzz <juzzle@mail.ru>
2024-07-26 19:02:04 +0300

    26/07/2024 - update users.classes -> add tickets Entity. Need TODO Users Interfaces, FacadeInterface, UserController 2

--- Java/src/main/java/di/model/entity/ticket/AbstractTicket.java
+++ Java/src/main/java/di/model/entity/ticket/AbstractTicket.java
@@ -2,16 +2,32 @@ package di.model.entity.ticket;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.text.SimpleDateFormat;
 
 @Data
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+
 @DiscriminatorColumn(name = "ticket_type", discriminatorType = DiscriminatorType.STRING)
-public  abstract  class AbstractTicket {
+public abstract class AbstractTicket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private double price;
+    @Column
+    private boolean valid = true;
+    @Column
     private String phoneNumber;
+    @Column
     private double discount;
+    @Column
+    private String dateStamp;
 
-
+    @PrePersist
+    protected void onCreate() {
+        dateStamp = new SimpleDateFormat("dd MMMM yyyy, HH:mm:ss").format(new java.util.Date());
+    }
 }
